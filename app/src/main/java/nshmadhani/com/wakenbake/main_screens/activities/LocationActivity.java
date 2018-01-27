d66177cd7f53246af8d1f05b6a902a53e49cf4ed
Nachiket Bhuta <nachiketbhuta@gmail.com>
2018-01-27 08:52:44 +0530

    Added Location Activity

---
+++ app/src/main/java/nshmadhani/com/wakenbake/main_screens/activities/LocationActivity.java
@@ -0,0 +1,20 @@
+package nshmadhani.com.wakenbake.main_screens.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nshmadhani.com.wakenbake.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+
+
+
+
+    }
+}
