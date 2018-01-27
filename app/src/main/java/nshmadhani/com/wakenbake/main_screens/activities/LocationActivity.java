ba26650d10e6e9761f38f0a099a9f2e644d01372
Nachiket Bhuta <nachiketbhuta@gmail.com>
2018-01-27 10:24:53 +0530

    Location Permission

--- app/src/main/java/nshmadhani/com/wakenbake/main_screens/activities/LocationActivity.java
+++ app/src/main/java/nshmadhani/com/wakenbake/main_screens/activities/LocationActivity.java
@@ -1,15 +1,32 @@
 package nshmadhani.com.wakenbake.main_screens.activities;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.skyfishjy.library.RippleBackground;
 
+import nshmadhani.com.wakenbake.Manifest;
 import nshmadhani.com.wakenbake.R;
 
 public class LocationActivity extends AppCompatActivity {
+
+    public LocationManager locationManager;
+    public LocationListener locationListener;
+
+    public RippleBackground rippleBackground;
+    public ImageView locationImageView;
+    public TextView gettingLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +34,61 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
 
 
-        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
-        ImageView imageView=(ImageView)findViewById(R.id.locationIcon);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        rippleBackground = findViewById(R.id.content);
+        locationImageView = findViewById(R.id.locationIcon);
+        gettingLocationTextView = findViewById(R.id.gettingLocation);
+
+        //Start Ripple Effect
+        rippleBackground.startRippleAnimation();
+
+        gettingLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        }
+    }
+
+    private void gettingLocation() {
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
             @Override
-            public void onClick(View view) {
-                rippleBackground.startRippleAnimation();
+            public void onLocationChanged(Location location) {
+
+                Log.i("Location", location.toString());
+
             }
-        });
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //Ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        }
     }
 }
