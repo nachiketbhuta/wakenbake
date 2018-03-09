7edc9060fc7bd3301210f7fca3a5433ecd02aa28
dishant123 <dishantchandura@gmail.com>
2018-03-09 10:01:14 +0530

    LogOut Dialog

---
+++ app/src/main/java/nshmadhani/com/wakenbake/fragments/LogoutFragment.java
@@ -0,0 +1,35 @@
+package nshmadhani.com.wakenbake.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogoutFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle("Logout")
+                .setMessage("Are you sure want to logout ?")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth auth = FirebaseAuth.getInstance();
+                        FirebaseUser user = auth.getCurrentUser();
+
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+
+    }
+}
