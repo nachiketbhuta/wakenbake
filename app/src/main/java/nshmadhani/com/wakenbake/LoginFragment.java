4d6d4ffd4711241bce611150990ce1434a5d46e8
nachiketbhuta <nachiketbhuta@gmail.com>
2018-03-20 00:07:11 +0530

    Colors added in colors.xml

--- app/src/main/java/nshmadhani/com/wakenbake/LoginFragment.java
+++ app/src/main/java/nshmadhani/com/wakenbake/LoginFragment.java
@@ -14,10 +14,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * Created by Nisha on 3/15/2018.
@@ -26,33 +39,48 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginFragment extends Fragment {
 
 
+    private static final int RC_SIGN_IN = 2;
     private TextInputLayout mUserNameTextInputLayout;
     private TextInputLayout mPasswordTextInputLayout;
     private Button mLoginButton;
-
+    private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-
     private LoginActivity mActivity;
-
-
     public static final String TAG = LoginFragment.class.getSimpleName();
+    public GoogleSignInAccount acct;
+    public GoogleApiClient mGoogleApiClient;
+    public SignInButton mGoogleButton;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.login_fragment, container, false);
 
         mUserNameTextInputLayout = rootView.findViewById(R.id.username_til);
         mPasswordTextInputLayout = rootView.findViewById(R.id.password_til);
         mLoginButton = rootView.findViewById(R.id.login_bt);
+        mGoogleButton = rootView.findViewById(R.id.googleButton);
 
-
+        mGoogleButton.setSize(SignInButton.SIZE_WIDE);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: click");
                 attemptLogin();
+            }
+        });
+
+        mGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: google button" );
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        signInWithGoogle();
+                    }
+                });
             }
         });
         return rootView;
@@ -63,7 +91,25 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mActivity = (LoginActivity) getActivity();
 
-        mAuth = mActivity.getmAuth();
+        if (mActivity != null) {
+            mAuth = mActivity.getmAuth();
+        }
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(mActivity)
+                .enableAutoManage(mActivity, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(mActivity, "OnConnectFailedListener", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
     public void toggleInputs(){
@@ -103,6 +149,54 @@ public class LoginFragment extends Fragment {
             Toast.makeText(getActivity().getApplicationContext(), "Enter both username and password", Toast.LENGTH_LONG).show();
         }
         toggleInputs();
+    }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(mActivity, "Google sign in failed", Toast.LENGTH_SHORT).show();
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(mActivity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            Intent intent = new Intent(mActivity, LocationActivity.class);
+                            startActivity(intent);
+                            mActivity.finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(mActivity, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 
